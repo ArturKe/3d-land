a52579843d62653c70f003a4adf18cd59b99562d
Artur <vashpartner@gmail.com>
2023-04-29 18:05:05 +0300

    Character movement

--- src/main.ts
+++ src/main.ts
@@ -43,6 +43,10 @@ function animation( time: number ) {
 	mesh.rotation.x = time / 2000;
 	mesh.rotation.y = time / 1000;
 
+  if (renderer.xr.isPresenting && selectPressed()){
+    moveDolly(time);
+  }
+
 	renderer.render( scene, camera );
 
 }
@@ -65,18 +69,27 @@ function setBackgroundColor (bgColor: string | number) {
 // }
 
 // --------------------------------------------------- //
-function onSelectStart(event: any) {    
-  // this.userData.selectPressed = true;
+let dolly = new THREE.Object3D(  );
+dolly.position.set(0, 0, 10);
+dolly.add( camera );
+let dummyCam = new THREE.Object3D();
+camera.add( dummyCam );
+scene.add( dolly )
+let origin = new THREE.Vector3();
+
+
+function onSelectStart(this: any, event: any) {    
+  this.userData.selectPressed = true;
   console.log(event)
 }
 
-function onSelectEnd() {
-  // this.userData.selectPressed = false;
+function onSelectEnd(this: any) {
+  this.userData.selectPressed = false;
   // console.log(event)
 }
 
 // let controllers = buildControllers(dolly);
-let controllers = buildControllers();
+let controllers = buildControllers(dolly);
 
 
 controllers.forEach((controller) => {
@@ -84,7 +97,7 @@ controllers.forEach((controller) => {
   controller.addEventListener( 'selectend', onSelectEnd );
 });
 
-function buildControllers( parent = scene ){
+function buildControllers( parent: THREE.Object3D = scene ){
   // debugger
   const controllerModelFactory = new XRControllerModelFactory();
   const geometry = new THREE.BufferGeometry().setFromPoints( [ new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, 0, -1 ) ] );
@@ -107,4 +120,81 @@ function buildControllers( parent = scene ){
   }
   
   return controllers;
+}
+
+// ----------------------------------------------- Dolly ----------------------------- //
+
+function selectPressed(){
+  return ( controllers !== undefined && (controllers[0].userData.selectPressed || controllers[1].userData.selectPressed) );    
+}
+
+
+
+function moveDolly(dt: number){
+  // if (this.proxy === undefined) return;
+  
+  // const wallLimit = 1.3;
+  const speed = 0.00001;
+  let pos = dolly.position.clone();
+  pos.y += 1;
+  
+  let dir = new THREE.Vector3();
+  //Store original dolly rotation
+  
+  const quaternion = dolly.quaternion.clone();
+
+  //Get rotation for movement from the headset pose
+  let qt = new THREE.Quaternion
+  // dolly.quaternion.copy(dummyCam.getWorldQuaternion(qt));
+  dummyCam.getWorldQuaternion(qt)
+  dolly.quaternion.copy(qt);
+  dolly.getWorldDirection(dir);
+  // dir.negate();
+  // raycaster.set(pos, dir);
+
+  // let blocked = false;
+
+  // let intersect = this.raycaster.intersectObject(this.proxy);
+  // if (intersect.length>0){
+  //     if (intersect[0].distance < wallLimit) blocked = true;
+  // }
+
+
+  dolly.translateZ(-dt*speed);
+  pos = dolly.getWorldPosition( origin );
+
+  //cast left
+  // dir.set(-1,0,0);
+  // dir.applyMatrix4(dolly.matrix);
+  // dir.normalize();
+  // this.raycaster.set(pos, dir);
+
+  // intersect = this.raycaster.intersectObject(this.proxy);
+  // if (intersect.length>0){
+  //     if (intersect[0].distance<wallLimit) this.dolly.translateX(wallLimit-intersect[0].distance);
+  // }
+
+  //cast right
+  // dir.set(1,0,0);
+  // dir.applyMatrix4(dolly.matrix);
+  // dir.normalize();
+  // this.raycaster.set(pos, dir);
+
+  // intersect = this.raycaster.intersectObject(this.proxy);
+  // if (intersect.length>0){
+  //     if (intersect[0].distance<wallLimit) this.dolly.translateX(intersect[0].distance-wallLimit);
+  // }
+
+  //cast down
+  // dir.set(0,-1,0);
+  // pos.y += 1.5;
+  // this.raycaster.set(pos, dir);
+  
+  // intersect = this.raycaster.intersectObject(this.proxy);
+  // if (intersect.length>0){
+  //     this.dolly.position.copy( intersect[0].point );
+  // }
+
+  // //Restore the original rotation
+  dolly.quaternion.copy( quaternion );
 }
